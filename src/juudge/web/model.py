df58409a1c4eb81d9b0773d98d9aa36902c449c8
Michel Albert <michel@albert.lu>
2024-11-10 16:02:22 +0100

    Add more docs

--- src/juudge/web/model.py
+++ src/juudge/web/model.py
@@ -1,33 +1,54 @@
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from juudge.model import MyMetadata
 
 
 class DocumentMetadata(BaseModel):
-    name: str
-    type: str
-    identifiers: dict[str, Any]
+    name: str = Field(description="The card name (if applicable)")
+    type: str = Field(
+        description="The type of docuemnt that contained the information"
+    )
+    identifiers: dict[str, Any] = Field(
+        description=(
+            "A dictionary of additional identifiers for the document. These "
+            "can be used to look up the document in external systems."
+        )
+    )
 
     @staticmethod
     def from_app_metadata(metadata: MyMetadata):
         return DocumentMetadata(
-            name=metadata["name"],
+            name=metadata.get("name", ""),
             type=metadata["type"],
-            identifiers=dict(metadata["identifiers"] or {}),
+            identifiers=dict(metadata.get("identifiers", {}) or {}),
         )
 
 
 class ContextInfo(BaseModel):
-    metadata: DocumentMetadata
-    raw_content: str
+    metadata: DocumentMetadata = Field(
+        description="Metadata about the document"
+    )
+    raw_content: str = Field(
+        description=(
+            "The raw content of the document. This contains the data as it was "
+            "used by the assistant. It may contain content that is not part of "
+            "the original text. The original unmodified text is available in "
+            "the metadata."
+        )
+    )
 
 
 class QueryResponse(BaseModel):
-    question: str
-    answer: str
-    contexts: list[ContextInfo]
+    question: str = Field(description="The question that was submitted")
+    answer: str = Field(description="The answer to the question")
+    contexts: list[ContextInfo] = Field(
+        description=(
+            "Additional context-information that was used to construct "
+            "the answer"
+        )
+    )
 
     @staticmethod
     def from_langchain_response(
