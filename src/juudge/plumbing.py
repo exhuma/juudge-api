e0f9ed3168d162a3e8f0f2d127799184ae73b05d
Michel Albert <michel@albert.lu>
2024-11-19 21:20:12 +0100

    dd some docstrings

--- src/juudge/plumbing.py
+++ src/juudge/plumbing.py
@@ -1,8 +1,18 @@
+"""
+Utility functions to connect and wire up the components of the Juudge system.
+"""
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_postgres import PGVector
 
 
 def create_store(database_dsn: str) -> PGVector:
+    """
+    Create a store for MTG cards using OpenAI embeddings and Postgres.
+
+    :param database_dsn: The DSN for the Postgres database.
+    :return: A PGVector store for MTG cards.
+    """
     return PGVector(
         embeddings=OpenAIEmbeddings(),
         collection_name="mtg-cards",
